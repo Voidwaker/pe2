1b2e478e8caf106bbce493f7d0db6ce442e1971f
Sigurd vaaler <sigvaa52915@stud.noroff.no>
2025-01-13 12:07:46 +0100

    Backup header setup

--- pe2/src/App.jsx
+++ pe2/src/App.jsx
@@ -1,14 +1,23 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 
 function App() {
   return (
-    <div>
+    <Router>
       <Header />
-      {}
-    </div>
+      <Routes>
+        <Route path="/" element={<h1>Welcome to Holihub!</h1>} />
+        <Route path="/venues" element={<h1>Venues Page</h1>} />
+        <Route path="/bookings" element={<h1>My Bookings</h1>} />
+        <Route path="/my-venues" element={<h1>My Venues</h1>} />
+        <Route path="/profile" element={<h1>Profile Page</h1>} />
+        <Route path="/login" element={<h1>Login/Register</h1>} />
+      </Routes>
+    </Router>
   );
 }
 
 export default App;
+
 
