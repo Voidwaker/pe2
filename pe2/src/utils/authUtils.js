517c503e6cf07ff9ebbb70867aeaa6b1f3402ee4
Sigurd vaaler <sigvaa52915@stud.noroff.no>
2025-01-23 22:19:56 +0100

    cleanded up unecessary files and tryiong to get rid of some code additions

--- pe2/src/utils/authUtils.js
+++
@@ -1,9 +0,0 @@
-export function logout() {
-    localStorage.removeItem('Token');
-    localStorage.removeItem('Profile');
-  
-    console.log('User logged out, localStorage cleared');
-  
-    window.location.href = '/';  
-  }
-  
