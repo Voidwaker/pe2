d7b97747280c4545fa17a54613b35b5f720bd0a6
Sigurd vaaler <sigvaa52915@stud.noroff.no>
2025-02-16 21:35:40 +0100

    testing
    tests

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
   plugins: [react()],
   base: "/", 
+  server: {
+    port: 5173,
+  },
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./src/setupTest.js", // Sjekk at denne peker riktig
+  },
 });
