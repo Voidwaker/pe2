ced3ff241dd3ccb67fa0dc2dabd82a67fea89ba0
Sigurd vaaler <sigvaa52915@stud.noroff.no>
2025-02-18 13:02:21 +0100

    Update constants.js
    removed comments from constants.js

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,22 +1,14 @@
 export const API_KEY = "4306f4b9-7186-498b-a4a7-c1c17566fbb6";
 export const API_BASE = "https://v2.api.noroff.dev";
-
-// Auth
 export const API_AUTH = "/auth";
 export const API_REGISTER = `${API_AUTH}/register`;
 export const API_LOGIN = `${API_AUTH}/login`;
 export const API_CREATE_API_KEY = `${API_AUTH}/create-api-key`;
-
-// Bookings
 export const API_BOOKINGS = "/holidaze/bookings";
 export const API_BOOKINGS_BY_PROFILE = (name) => `/holidaze/profiles/${name}/bookings?_venue=true`;
-
-// Profiles
 export const API_PROFILES = "/holidaze/profiles";
 export const API_PROFILE_BY_NAME = (name) => `/holidaze/profiles/${name}`;
 export const API_PROFILE_VENUES = (name) => `/holidaze/profiles/${name}/venues?_bookings=true`;
-
-// Venues
 export const API_VENUES = "/holidaze/venues";
 export const API_VENUE_BY_ID = (id) => `/holidaze/venues/${id}`;
 export const API_SEARCH_VENUES = (query) => `/holidaze/venues/search?q=${query}`;
